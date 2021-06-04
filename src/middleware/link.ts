b30bf728543595a0e983d2c78ef1a33a46183abb
Alexander Granhof <alexander.g737@gmail.com>
2021-06-04 16:16:50 +0200

    created a link middleware to link a wow char to a discord account

---
+++ src/middleware/link.ts
@@ -0,0 +1,28 @@
+import { Middleware } from '../services/pipeline'
+import { Database } from '../services/db'
+import slug from 'slug'
+
+const DatabaseConnection = new Database().connect()
+
+export const Link: Middleware = async (context, message, next) => {
+    const db = await DatabaseConnection
+
+    const isDM = message.channel.type === 'dm'
+    const isLinkCommand = message.content.startsWith('!link')
+
+    if (!isDM || !isLinkCommand) {
+        return next()
+    }
+
+    const [, character, ...rest] = message.content.split(' ')
+    const realm = rest.join(' ')
+
+    if (!character || !realm) {
+        return next()
+    }
+
+    await db.addClient(message.author.id, character, realm)
+    await message.reply(`${slug(character)}-${slug(realm)} has been linked to your discord account`)
+
+    return next()
+}
