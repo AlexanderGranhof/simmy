cf61351c09044729d2b2925a18e0bb8663019e16
Alexander Granhof <alexander.g737@gmail.com>
2021-06-04 16:15:47 +0200

    created a database to store clients and contestants

---
+++ src/services/db.ts
@@ -0,0 +1,105 @@
+import lowdb from 'lowdb'
+import FileAsync from 'lowdb/adapters/FileAsync'
+import path from 'path'
+import slug from 'slug'
+import crypto from 'crypto'
+
+const sha256 = (d: string) => crypto.createHash('sha256').update(d).digest('hex')
+
+type Client = {
+    discordID: string
+    realm: string
+    character: string
+}
+
+type Contestant = {
+    id: string,
+    character: string,
+    realm: string
+}
+
+type DatabaseModel = {
+    clients: Client[],
+    contestants: Contestant[]
+}
+
+const initialDB: DatabaseModel = {
+    clients: [],
+    contestants: []
+}
+
+export class Database {
+    private _DB_STORE_LOCATION: string
+    private db: lowdb.LowdbAsync<DatabaseModel>
+
+    constructor() {
+        this._DB_STORE_LOCATION = path.resolve(__dirname, '../../', 'database.json')
+        this.db = null as any
+    }
+
+    private verifyConnected() {
+        if (this.db === null) throw new Error('database is not connected')
+    }
+
+    async connect() {
+        this.db = await lowdb(new FileAsync<DatabaseModel>(this._DB_STORE_LOCATION))
+
+        await this.db.defaults<DatabaseModel>(initialDB).write()
+
+        return this
+    }
+
+    async addClient(discordID: string, character: string, realm: string) {
+        this.verifyConnected()
+
+        const newData = { discordID, character: slug(character), realm: slug(realm) }
+
+        const clients = await this.db.get('clients')
+        const exists = !!await clients.find({ discordID }).value()
+
+
+        if (exists) {
+            await clients.find({ discordID }).assign(newData).value()
+            await this.db.write()
+            return
+        }
+        
+        await clients.push(newData).value()
+        await this.db.write()
+    }
+
+    async getClient(discordID: string) {
+        this.verifyConnected()
+        await this.db.read()
+
+        return await this.db.get('clients').find({ discordID }).value()
+    }
+
+    async addContestant(character: string, realm: string) {
+        this.verifyConnected()
+
+        const id = sha256(slug(character) + slug(realm))
+
+        const newData = { id, character: slug(character), realm: slug(realm) }
+
+        const clients = await this.db.get('contestants')
+        const exists = !!await clients.find({ id }).value()
+
+
+        if (exists) {
+            await clients.find({ id }).assign(newData).value()
+            await this.db.write()
+            return
+        }
+        
+        await clients.push(newData).value()
+        await this.db.write()
+    }
+
+    async getContestants() {
+        this.verifyConnected()
+        await this.db.read()
+        
+        return await this.db.get('contestants').value()
+    }
+}
