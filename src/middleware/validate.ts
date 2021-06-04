4b83c8e97df625b833dd1c5063ff4c077eceb84c
Alexander Granhof <alexander.g737@gmail.com>
2021-06-04 16:15:53 +0200

    removed logging

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -4,7 +4,5 @@ export const Validate: Middleware = (context, message, next) => {
     const isBot = message.author.bot
     const notCommand = !message.content.startsWith('!')
 
-    console.log(isBot, notCommand)
-
     return next(isBot || notCommand)
 }
