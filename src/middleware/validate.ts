b27263daa62afcdaeced90de23b9333356d2cb81
Alexander Granhof <alexander.g737@gmail.com>
2021-06-04 15:25:06 +0200

    allow non admins to use commands

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -3,7 +3,8 @@ import { Middleware } from '../services/pipeline'
 export const Validate: Middleware = (context, message, next) => {
     const isBot = message.author.bot
     const notCommand = !message.content.startsWith('!')
-    const notAdmin = message.author.id !== process.env.ADMIN_DISCORD_ID
 
-    return next(isBot || notCommand || notAdmin)
+    console.log(isBot, notCommand)
+
+    return next(isBot || notCommand)
 }
